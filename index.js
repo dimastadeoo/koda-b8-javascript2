f6c107ddf1a1348f2589a6f07bba9fb49c613a30
dimastadeoo <53636907+dimastadeoo@users.noreply.github.com>
2026-05-15 10:13:56 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const angka = [20,65,43,12,54]
+
+let i = 0
+let max = angka[0]
+let min = angka[0]
+let total = 0
+
+while (i < angka.length){
+        if (angka[i] > max) {
+            max = angka[i]       
+        }
+
+        if (angka[i] < min){
+            min = angka[i]       
+        }
+        total += angka[i]
+
+        i++
+}
+console.log(`nilai  dari array: ${max}`)
+console.log(`nilai terkecil dari array: ${min}`)
+console.log(`rata rata nilai array: ${total/angka.length}`)
